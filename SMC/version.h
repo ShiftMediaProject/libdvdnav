229d78062415f1b082058d2b1075374e2241c0c0
Matthew Oliver <protogonoi@gmail.com>
2014-08-30 01:04:36 +1000

    Update VS projects for recent merge.

--- SMC/version.h
+++ SMC/version.h
@@ -1,9 +1,8 @@
-
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libdvdnav 5.0.0"
+#define PACKAGE_STRING "libdvdnav 5.0.1"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.0.0"
+#define PACKAGE_VERSION "5.0.1"
 
 /* Version number of package */
-#define VERSION "5.0.0"
+#define VERSION "5.0.1"
